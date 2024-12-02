840e0221bca52c4ac4a7d336f08b3de384563948
suk5dollar <suk@gmx.us>
2024-12-02 03:28:16 +0000

    ..

--- suk-kickstart/lua/custom/plugins/nvim-tree.lua
+++ suk-kickstart/lua/custom/plugins/nvim-tree.lua
@@ -1,9 +1,9 @@
-
-
 return {
-    "nvim-tree/nvim-tree.lua",
-    lazy = false,
-    config = {
-        filters = {},
-    }
+  'nvim-tree/nvim-tree.lua',
+  lazy = false,
+  config = {
+    filters = {
+      git_ignored = false,
+    },
+  },
 }
